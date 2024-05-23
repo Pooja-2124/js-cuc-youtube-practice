89d81c47ea10c437bddb5a21db50d80ea542fd41
Pooja-2124 <gadhawepooja2@gmail.com>
2024-05-23 10:11:33 +0000

    datatype conversion

---
+++ 01_basics/03_datatypeConverter.js
@@ -0,0 +1,29 @@
+// let a=null;
+// let b=Number(a);
+// console.log(b);
+// console.log(typeof b);
+
+// let a;
+// let b=Number(a);
+// console.log(b);
+// console.log(typeof b);
+
+// let a=null;
+// let b=String(a);
+// console.log(b);
+// console.log(typeof b);
+
+// let a;
+// let b=String(a);
+// console.log(b);
+// console.log(typeof b);
+
+// let a=null;
+// let b=Boolean(a);
+// console.log(b);
+// console.log(typeof b);
+
+// let a;
+// let b=Boolean(a);
+// console.log(b);
+// console.log(typeof b);

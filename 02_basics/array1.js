ceff8dd5f20bea51e63a26ce650fac3250408743
Pooja-2124 <gadhawepooja2@gmail.com>
2024-07-30 16:32:24 +0000

    array2.js

--- 02_basics/array1.js
+++ 02_basics/array1.js
@@ -5,10 +5,20 @@ arr1=[1,2,3,4,5];
 // arr1.pop();
 // arr1.pop();
 // console.log(arr1);
-arr1.unshift(7);
-arr1.unshift(8);
+// arr1.unshift(7);
+// arr1.unshift(8);
+// console.log(arr1);
+// arr1.shift();
+// arr1.shift();
+// arr1.shift();
 console.log(arr1);
-arr1.shift();
-arr1.shift();
-arr1.shift();
+arr2=["apple","banana","orange"];
+console.log(arr2);
+const newarr=arr1.join()
+console.log(newarr);
+const newarr1=arr1.slice(1,3);//does not make changes in orignal array (by value)
+console.log(newarr1);
+console.log(arr1);
+const newarr2=arr1.splice(1,3);//make changes in orignal array (by reference)
+console.log(newarr2);
 console.log(arr1);
